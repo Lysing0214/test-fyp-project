bdecdd97ceca854f21fdba7ea6d99beda34acedb
Lysing0214 <lysing213@gmail.com>
2025-03-11 17:22:24 +0700

    mv service_key to .env.local

--- src/firebaseAdmin.ts
+++ src/firebaseAdmin.ts
@@ -1,11 +1,18 @@
 import admin from "firebase-admin";
-import { readFileSync } from "fs";
+// import { readFileSync } from "fs";
+import dotenv from "dotenv";
 
 // Check if Firebase Admin is already initialized
 if (!admin.apps.length) {
-  const serviceAccount = JSON.parse(
-    readFileSync("service_key.json", "utf8")
-  );
+
+  // const serviceAccount = JSON.parse(
+  //   readFileSync("service_key.json", "utf8")
+  // );
+
+  // Load environment variables from .env.local file
+  dotenv.config();
+
+  const serviceAccount = JSON.parse(process.env.SERVICE_KEY_JSON as string);
 
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
